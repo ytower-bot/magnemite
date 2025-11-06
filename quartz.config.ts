3e46608c4701d7cf859eb01b1e9221245eb2742f
ytower <ytower@proton.me>
2025-11-06 01:05:18 -0300

    first deploy?????

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://ytower-bot.github.io/magnemite/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
